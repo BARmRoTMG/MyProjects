c181d5cbdfc87bec28fbee09ba109169b517c384
BARmRoTMG <barbar903@walla.com>
2022-04-26 16:45:00 +0300

    TEST2

--- Loops_26.4/Program.cs
+++ Loops_26.4/Program.cs
@@ -10,12 +10,58 @@ namespace Loops_26._4
     {
         static void Main(string[] args)
         {
-            for (int i = 99; i >= 0; i -= 11)
+            Console.WriteLine("Which course would you like to sign to?");
+            Console.WriteLine("1. Development course. \n2. QA course \n3. Talk to a consultant");
+
+            int studentChoice = int.Parse(Console.ReadLine());
+            int Dev = 0;
+            int QA = 0;
+            int Consultant = 0;
+
+            switch (studentChoice)
             {
-                Console.WriteLine(i);
+                case 1:
+                    Dev++;
+                    break;
+                case 2:
+                    QA++;
+                    break;
+                case 3:
+                    Consultant++;
+                    break;
             }
 
+
+
+
+
+
+
+
+
+
+
+
+            bool bStudentChoice = int.TryParse(Console.ReadLine(), out studentChoice);
+
+            while (!bStudentChoice)
+            {
+                if (bStudentChoice)
+                {
+                    bStudentChoice = true;
+                    StudentRunner();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid data type!");
+                }
+            }
             Console.ReadKey();
+        }
+        private static void StudentRunner()
+        {
+            Console.WriteLine("test");
         }
     }
 }
