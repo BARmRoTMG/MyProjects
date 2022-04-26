53fa5fe43722fbe0d7af5782f88d2ff8285f9237
BARmRoTMG <barbar903@walla.com>
2022-04-26 17:09:13 +0300

    Students Course Counter Program

--- Loops_26.4/Program.cs
+++ Loops_26.4/Program.cs
@@ -10,58 +10,56 @@ namespace Loops_26._4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Which course would you like to sign to?");
-            Console.WriteLine("1. Development course. \n2. QA course \n3. Talk to a consultant");
-
-            int studentChoice = int.Parse(Console.ReadLine());
+            int numOfStudents = 10;
+            int studentChoice;
             int Dev = 0;
             int QA = 0;
             int Consultant = 0;
 
-            switch (studentChoice)
+            for (int i = 1; i < numOfStudents; i++)
             {
-                case 1:
-                    Dev++;
-                    break;
-                case 2:
-                    QA++;
-                    break;
-                case 3:
-                    Consultant++;
-                    break;
-            }
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Student number " + i + ".");
+                Console.WriteLine("Which course would you like to sign to?");
+                Console.WriteLine("1. Development course. \n2. QA course \n3. Talk to a consultant");
 
+                bool bStudentChoice = int.TryParse(Console.ReadLine(), out studentChoice);
 
-
-
-
-
-
-
-
-
-
-
-            bool bStudentChoice = int.TryParse(Console.ReadLine(), out studentChoice);
-
-            while (!bStudentChoice)
-            {
                 if (bStudentChoice)
                 {
-                    bStudentChoice = true;
-                    StudentRunner();
-                }
-                else
+                    if (studentChoice == 1 || studentChoice == 2 || studentChoice == 3)
+                    {
+                        switch (studentChoice)
+                        {
+                            case 1:
+                                Dev++;
+                                break;
+                            case 2:
+                                QA++;
+                                break;
+                            case 3:
+                                Consultant++;
+                                break;
+                        }
+
+                    } else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Invalid answer!");
+                        i--;
+                    }
+                } else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Invalid data type!");
+                    Console.WriteLine("Invalid answer!");
+                    i--;
                 }
             }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{Dev} students would like to take the Dev course, \n{QA} students would like to take the QA course, \n{Consultant} would like to talk to a consultant.");
+
             Console.ReadKey();
-        }
-        private static void StudentRunner()
-        {
-            Console.WriteLine("test");
         }
     }
 }
