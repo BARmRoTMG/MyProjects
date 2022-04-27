b8c8ccfb41f4b6f5b5027498d9af0ee2c0e7b2d3
BARmRoTMG <barbar903@walla.com>
2022-04-27 21:54:29 +0300

    Loops and fibonacci

---
+++ Fibonacci Sequence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Sequence
+{
+    internal class Program
+    {
+        public static int FibonacciSeries(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1; 
+            return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the Fibonacci Series: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("{0} ", FibonacciSeries(i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LoopsHomeWork/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCISE 3.1
+
+            //int fact = 1;
+            //int number;
+
+            //Console.Write("Enter any number: ");
+            //number = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    fact = fact * i;
+            //}
+
+            //Console.Write("Factorial of " + number + " is: " + fact);
+
+
+            // EXERCISE 3.2
+
+            //int baseN;
+            //int power;
+            //bool running = true;
+
+            //while (running)
+            //{
+            //    Console.ForegroundColor = ConsoleColor.White;
+            //    Console.Write("Enter base number: ");
+            //    bool BbaseN = int.TryParse(Console.ReadLine(), out baseN);
+            //    Console.Write("Enter power number: ");
+            //    bool Bpower = int.TryParse(Console.ReadLine(), out power);
+
+            //    if (Bpower && BbaseN)
+            //    {
+            //        double result = Math.Pow(baseN, power);
+
+            //        Console.ForegroundColor = ConsoleColor.Yellow;
+            //        Console.WriteLine($"{baseN} in the power of {power} is: {result}");
+            //        running = false;    
+            //    }
+            //    else
+            //    {
+            //        running = true;
+            //        Console.ForegroundColor = ConsoleColor.Red;
+            //        Console.WriteLine("Invalid data type!");
+            //    }
+
+            //}
+
+
+            // EXERCISE 3.3
+
+            Random random = new Random();
+
+            int devBy3 = 0;
+
+            string output = "";
+
+            for (int i = 0; i < 10 && devBy3 < 3; i++)
+            {
+                //Console.WriteLine(rnd = random.Next(1, 101));
+                int rnd = random.Next(1, 101);
+
+                output += " " + rnd;
+
+                if (rnd % 3 == 0)
+                {
+                    devBy3++;
+                }
+            }
+
+            if (devBy3 == 3)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Program Failed.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
