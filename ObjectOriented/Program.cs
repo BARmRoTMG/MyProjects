a9fb54e294063b3587e27cf9c531dd90212edc43
BARmRoTMG <barbar903@walla.com>
2022-06-23 14:13:43 +0300

    23.6

---
+++ ObjectOriented/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOriented
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Bar", 23);
+            Person p2 = new Person("Bar", 24);
+            bool b = p1.Equals(p2);
+
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(p2.ToString());
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
