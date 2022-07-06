835f753f2d86945099009ebd4cdc7faf2e728d27
BARmRoTMG <barbar903@walla.com>
2022-07-06 14:59:08 +0300

    Hint Button doesnt work PROPERLY

--- HangedMan/MainPage.xaml.cs
+++ HangedMan/MainPage.xaml.cs
@@ -21,6 +21,13 @@ namespace HangedMan
 
         string _word;
         int _counter = 0;
+        int difficultyLevel = 0;
+
+        public int DifficultyLevel
+        {
+            get { return difficultyLevel; }
+            set { difficultyLevel = value; }
+        }
 
         public MainPage()
         {
@@ -28,7 +35,7 @@ namespace HangedMan
             IntializeGame();
         }
 
-        public void IntializeGame()
+        public void IntializeGame() //New Game Creation
         {
             InitializeWindow();
             _images = new List<BitmapImage>();
@@ -36,16 +43,16 @@ namespace HangedMan
             CreateWordArea();
         }
 
-        public void InitializeWindow()
+        public void InitializeWindow() //UI/Screen Initializer
         {
             ApplicationView view = ApplicationView.GetForCurrentView();
             view.Title = "Bar Mashiach";
             ApplicationView.PreferredLaunchViewSize = new Size(1920, 1080);
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
-            canvas.Background = new ImageBrush { ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/chalkboard.png")) };
+            canvas.Background = new ImageBrush { ImageSource = new BitmapImage(new Uri("ms-appx:///Assets/background.jpg")) };
         }
 
-        private void CreateKeyboard()
+        private void CreateKeyboard() //Screen Keyboard
         {
             _buttons = new List<Button>();
 
@@ -75,7 +82,7 @@ namespace HangedMan
             }
         }
 
-        private void LoadImage()
+        private void LoadImage() //Image changer
         {
             for (int i = 0; i < 11; i++)
             {
@@ -87,18 +94,27 @@ namespace HangedMan
         private string WordGenerator()
         {
             string[] easy = { "chicken", "football", "house", "television", "sugar", "elephant" };
-            string[] normal = { "conservation", "strength", "production", "eliminate", "suspect", "character" };
+            string[] normal = { "conversation", "strength", "production", "eliminate", "suspect", "character" };
             string[] hard = { "achievement", "violation", "continuous", "exhibition", "pedestrian", "atmosphere" };
+
             Random rnd = new Random();
+
+            if (difficultyLevel == 0)
+                return easy[rnd.Next(easy.Length)];
+            else if (difficultyLevel == 1)
+                return normal[rnd.Next(normal.Length)];
+            else if (difficultyLevel == 2)
+                return hard[rnd.Next(hard.Length)];
+
             return easy[rnd.Next(easy.Length)];
-        }
+        } //Word generator by difficulty
         
-        private void CreateWordArea()
+        private void CreateWordArea() //Initializing words
         {
             _counter = 0;
             CreateKeyboard();
             this._word = WordGenerator();
-            imageMiss.Source = _images[0];
+            imageHolder.Source = _images[0];
             _charField = new List<TextBlock>();
             wordArea.Children.Clear(); 
             
@@ -116,7 +132,7 @@ namespace HangedMan
             _charField[0].Text = this._word[0].ToString(); // First char of word
             _charField[this._word.Length - 1].Text = this._word[this._word.Length - 1].ToString(); // Last char of word
         }
-        private void Keyboard_Click(object sender, RoutedEventArgs e)
+        private void Keyboard_Click(object sender, RoutedEventArgs e) //Keybaord Input
         {
             Button button = sender as Button;
             string character = button.Content.ToString();
@@ -134,7 +150,7 @@ namespace HangedMan
             if (hit == false)
             {
                 _counter += 1;
-                imageMiss.Source = _images[_counter];
+                imageHolder.Source = _images[_counter];
             }
 
             // Lose
@@ -158,7 +174,7 @@ namespace HangedMan
             button.IsEnabled = false;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e) //New Game Button
         {
             CreateWordArea();
         }
@@ -168,6 +184,37 @@ namespace HangedMan
             MessageDialog messageDialog = new MessageDialog("Play again", s);
             await messageDialog.ShowAsync();
             CreateWordArea(); // Starts new game
+        } //General Play Again Button to Dialog Box
+        private void difficulty_Click(object sender, RoutedEventArgs e)
+        {
+            difficultyLevel++;
+            textHolder.Text = "Click on 'New Game' for the difficulty change to take effect.";
+
+            switch (difficultyLevel)
+            {
+                case 0:
+                    difficulty.Content = "Easy";
+                    difficultyLevel = 0;
+                    break;
+                case 1:
+                    difficulty.Content = "Normal";
+                    difficultyLevel = 1;
+                    break;
+                case 2:
+                    difficulty.Content = "Hard";
+                    difficultyLevel = 2;
+                    break;
+                default:
+                    difficulty.Content = "Easy";
+                    difficultyLevel = 0;
+                    break;
+            }
+        } //Dynamic Difficulty Button
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) //Hint Button
+        {
+            Random r = new Random();
+            _charField[r.Next(this._word.Length)].Text = this._word[r.Next(this._word.Length - 1)].ToString();
         }
     }
 }
