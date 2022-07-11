d85dcb43b21f930bb2c161db3cc7c5387031cd40
BARm <barbar903@walla.com>
2022-07-11 13:45:54 +0300

    שי 11.7

--- ShayClass/Program.cs
+++ ShayClass/Program.cs
@@ -10,18 +10,25 @@ namespace ShayClass
     {
         static void Main(string[] args)
         {
-            int[] a = { 10, 12, 3, 9, 7 , 2, 8};
-            int[] b = { 7, 45, 33, 4, 23, 9, 1 };
+            //int[] a = { 1, 3, 4, 6, 8 , 9 };
+            //int[] b = { 2, 4, 7, 10, 12 };
+            //int[] c = new int[a.Length + b.Length];
 
-            //BubbleSort(a);
-            //SortK(a, 3);
-            //MaxSort(a);
-            InsertionSort(a);
+            ////BubbleSort(a);
+            ////SortK(a, 3);
+            ////MaxSort(a);
+            ////InsertionSort(a);
+            ////Merge(a, b, c);
+            //MergeSort(a, 9, 5);
 
-            Console.WriteLine(f(a, b, 9));
 
-            for (int i = 0; i < a.Length; i++)
-                Console.Write(a[i] + " ");
+            //Console.WriteLine(f(a, b, 9));
+
+            int[] arr = { 1, 2, 3, 4, 6, 3, 5, 7, 8 };
+            MergeSort(arr, 0, arr.Length - 1);
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
         }
 
         public static void BubbleSort(int[] a)
@@ -164,6 +171,89 @@ namespace ShayClass
                     return true;
             }
             return false;
+        }
+
+
+
+
+        // 11/07/2022
+
+        public static void Merge(int[] a, int[] b, int[] c)
+        {
+            int i, j, k;
+            i = j = k = 0;
+
+            while (i < a.Length && i < b.Length)
+            {
+                if (a[i] < b[j])
+                {
+                    c[k] = a[i];
+                    i++;
+                } else
+                {
+                    c[k] = b[j];
+                    j++;
+                }
+                k++;
+            }
+
+            int[] temp;
+            int index;
+
+            if (i == a.Length)
+            {
+                temp = b;
+                index = j;
+            } else
+            {
+                temp = a;
+                index = i;
+            }
+
+            for (; index < a.Length; index++)
+                c[k] = temp[index];
+        }
+
+        public static void MergeSort(int[] a, int begin, int end)
+        {
+            if (begin >= end)
+                return;
+
+            MergeSort(a, begin, (begin + end) / 2);
+            MergeSort(a, 1 + (begin + end) / 2, end);
+            Merge(a, begin, end);
+        }
+
+        public static void Merge(int[] a, int begin, int end)
+        {
+            int[] b = new int[(end - begin) + 1];
+            int mid = 1 + (begin + end) / 2;
+            int i, k;
+            i = begin;
+            k = 0;
+
+            while(i < 1 + (begin + end) / 2 && mid <= end)
+            {
+                if (a[i] < a[mid])
+                {
+                    b[k] = a[i];
+                    i++;
+                } else
+                {
+                    b[k] = a[mid];
+                    mid++;
+                }
+                k++;
+            }
+
+            if (i == 1 + (begin + end) / 2)
+                for (; mid <= end; mid++, k++)
+                    b[k] = a[mid];
+            else
+                for (; i < 1 + (begin + end) / 2; i++, k++)
+                    b[k] = a[i];
+            for (i = begin, k = 0; k < b.Length; i++, k++)
+                a[i] = b[k];
         }
     }
 }
