363978c644656321e1fd1ccb74c02bfaee24ccc1
BARmRoTMG <barbar903@walla.com>
2022-11-07 00:01:10 +0200

    nbh

--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -1,120 +1,160 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
 
-namespace DoFactory.GangOfFour.Factory.Structural
+//namespace Composite.Structural
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            // Create a tree structure
+//            Composite root = new Composite("root");
+//            root.Add(new Leaf("Leaf A"));
+//            root.Add(new Leaf("Leaf B"));
+
+//            Composite comp = new Composite("Composite X");
+//            comp.Add(new Leaf("Leaf XA"));
+//            comp.Add(new Leaf("Leaf XB"));
+
+//            root.Add(comp);
+//            root.Add(new Leaf("Leaf C"));
+
+
+//            // Add and remove a leaf
+//            Leaf leaf = new Leaf("Leaf D");
+//            root.Add(leaf);
+//            root.Remove(leaf);
+
+
+//            // Recursively display tree
+//            root.Display(1);
+
+
+//            // Wait for user
+//            Console.ReadKey();
+//        }
+//    }
+
+//    public interface IComponent
+//    {
+//        string Name { get; set; }
+//        void Add(IComponent c);
+//        void Remove(IComponent c);
+//        void Display(int depth);
+//    }
+
+
+//    public class Composite : IComponent
+//    {
+//        List<IComponent> children = new List<IComponent>();
+//        public string Name { get; set; }
+
+//        // Constructor
+//        public Composite(string name)
+//        {
+//            Name = name;
+//        }
+
+//        public void Add(IComponent component)
+//        {
+//            children.Add(component);
+//        }
+
+//        public void Remove(IComponent component)
+//        {
+//            children.Remove(component);
+//        }
+
+//        public void Display(int depth)
+//        {
+//            Console.WriteLine(new String('-', depth) + Name);
+
+//            // Recursively display child nodes
+//            foreach (IComponent component in children)
+//                component.Display(depth + 2);
+//        }
+//    }
+
+
+//    public class Leaf : IComponent
+//    {
+//        public string Name { get; set; }
+
+//        // Constructor
+//        public Leaf(string name)
+//        {
+//            Name = name;
+//        }
+
+//        public void Add(IComponent c)
+//        {
+//            Console.WriteLine("Cannot add to a leaf");
+//        }
+
+//        public void Remove(IComponent c)
+//        {
+//            Console.WriteLine("Cannot remove from a leaf");
+//        }
+
+//        public void Display(int depth)
+//        {
+//            Console.WriteLine(new String('-', depth) + Name);
+//        }
+//    }
+//} Composite
+
+using System;
+
+namespace Proxy.Structural
 {
     /// <summary>
-    /// MainApp startup class for Structural 
-    /// Factory Method Design Pattern.
+    /// Proxy Design Pattern
     /// </summary>
 
-    class MainApp
+    public class Program
     {
-        /// <summary>
-        /// Entry point into console application.
-        /// </summary>
-
-        static void Main()
+        public static void Main(string[] args)
         {
-            // An array of creators
-            var creators = Init();
+            // Create proxy and request a service
 
-
-            // Iterate over creators and create products
-
-            foreach (var creator in creators)
-            {
-                var product = creator.Create();
-                Console.WriteLine("Created {0}",
-                          product.GetType().Name);
-            }
+            var proxy = new Proxy();
+            proxy.Request();
 
             // Wait for user
 
-            Console.ReadLine();
+            Console.ReadKey();
         }
+    }
 
-        private static IProductCreator[] Init()
+    public interface ISubject
+    {
+        public void Request();
+    }
+
+    public class RealSubject : ISubject
+    {
+        public void Request()
         {
-            return new IProductCreator[]
+            Console.WriteLine("Called RealSubject.Request()");
+        }
+    }
+
+    public class Proxy : ISubject
+    {
+        private RealSubject realSubject;
+
+        public void Request()
+        {
+            // Use 'lazy initialization'
+
+            if (realSubject == null)
             {
-                new CreatorA(),
-                new CreatorB(),
-                new CreatorC()
-            };
-        }
-    }
+                realSubject = new RealSubject();
+            }
 
-    /// <summary>
-    /// The 'Product' abstract class
-    /// </summary>
-
-    interface IProduct
-    {
-    }
-
-    /// <summary>
-    /// A 'ConcreteProduct' class
-    /// </summary>
-
-    class ConcreteProductA : IProduct
-    {
-    }
-
-    /// <summary>
-    /// A 'ConcreteProduct' class
-    /// </summary>
-
-    class ConcreteProductB : IProduct
-    {
-    }
-
-    class ConcreteProductC : IProduct
-    {
-
-        public ConcreteProductC(string name)
-        {
-
-        }
-    }
-
-    /// <summary>
-    /// The 'Creator' abstract class
-    /// </summary>
-
-    interface IProductCreator
-    {
-        IProduct Create();
-    }
-
-    /// <summary>
-    /// A 'ConcreteCreator' class
-    /// </summary>
-
-    class CreatorA : IProductCreator
-    {
-        public IProduct Create()
-        {
-            return new ConcreteProductA();
-        }
-    }
-
-    /// <summary>
-    /// A 'ConcreteCreator' class
-    /// </summary>
-
-    class CreatorB : IProductCreator
-    {
-        public IProduct Create()
-        {
-            return new ConcreteProductB();
-        }
-    }
-
-    class CreatorC : IProductCreator
-    {
-        public IProduct Create()
-        {
-            return new ConcreteProductC("World");
+            realSubject.Request();
         }
     }
 }
+
+
