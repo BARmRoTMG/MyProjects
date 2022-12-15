91cd0ad4448f223c88cf5939aef83fafbe5e42d8
BARm <barbar903@walla.com>
2022-12-15 11:29:07 +0200

    new pc

---
+++ ApiSample/Controllers/PetsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSample.Controllers
+{
+    [ApiController]
+    [Route("api/pets")]
+    public class PetsController : ControllerBase
+    {
+        public PetsController()
+        {
+
+        }
+
+        [HttpGet]
+        public List<Pet> Index()
+        {
+            return new List<Pet>
+            {
+                new Pet {Name = "Tofy"}
+            };
+        }
+
+        [HttpGet("{id}")]
+        public Pet Get(int id)
+        {
+            return new Pet { Id= id , Name = "Tofy"};
+        }
+    }
+
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
