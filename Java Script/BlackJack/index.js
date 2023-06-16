9fd1b3a3585857454766dab53ffe1f7bf4ecc545
BARm <barbar903@walla.com>
2023-06-16 14:01:03 +0300

    Chrome Extension(JS)

--- Java Script/BlackJack/index.js
+++ Java Script/BlackJack/index.js
@@ -10,6 +10,7 @@ let dealerSum = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
+
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
